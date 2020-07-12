17771867b7b3ee8f58d94987c81e83765458b062
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-07-12 12:58:58 +0200

    Song model

---
+++ MusicMaster/UserCommandLogic/Song.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace MusicMasterBot
+{
+    public class Song
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Album { get; set; }
+
+        public int Year { get; set; }
+
+        public ISet<string> Genres { get; set; }
+
+        public ISet<Song> BestMatches { get; set; }
+
+        public bool ArtistInDatabase { get; set; }
+
+        public bool TitleInDatabase { get; set; }
+
+        public void UpdateByDatabase()
+        {
+            /// find the song in the database based on title, artist; if not found, note the best matches for artist and title with their best matches for the other parameter respectively
+        }
+    }
+}
