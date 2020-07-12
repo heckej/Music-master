8c43e05c005e4c0402fdfb9a638a15e8bfdd58b7
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-07-12 13:26:22 +0200

    Metrics: character counter

---
+++ MusicMaster/Metrics/Characters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metrics
+{
+    public static class Characters
+    {
+        public static IDictionary<char, int> Count(string str)
+        {
+            IDictionary<char, int> chars = new Dictionary<char, int>();
+            foreach (char chr in str)
+            {
+                int oldVal = 0;
+                chars.TryGetValue(chr, out oldVal);
+                chars.Add(chr, oldVal + 1);
+            }
+            return chars;
+        }
+
+
+    }
+}
