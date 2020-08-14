2097b0178a934f1e9d50cd055f6ce51e9212c8a0
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-14 11:19:04 +0200

    Basic music player class skeleton

---
+++ MusicMaster/Tools/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using MusicMasterBot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools
+{
+    public class MusicPlayer
+    {
+        Song _currentSong;
+        bool _playing;
+
+        public void Play(Song song)
+        {
+            // play the song song.Title by song.Artist
+            _currentSong = song;
+            _playing = true;
+        }
+
+        public void Pause()
+        {
+            // pause current song
+            _playing = false;
+        }
+
+        public void Resume()
+        {
+            // resume playing
+            _playing = true;
+        }
+    }
+}
