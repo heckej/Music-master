988eafa1d08fb9637c7c75ce6622c1bc919315cb
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-14 11:16:35 +0200

    Method to take the difference of 2 dictionaries with chars

--- MusicMaster/Metrics/Characters.cs
+++ MusicMaster/Metrics/Characters.cs
@@ -28,6 +28,35 @@ namespace Metrics
             return chars;
         }
 
+        /// <summary>
+        /// Calculates the difference
+        /// </summary>
+        /// <param name="dict1"></param>
+        /// <param name="dict2"></param>
+        /// <returns></returns>
+        public static IDictionary<char, int> DifferenceOccurrences(IDictionary<char, int> dict1, IDictionary<char, int> dict2)
+        {
+            IDictionary<char, int> difference = new Dictionary<char, int>();
+            foreach (var character in dict1.Keys)
+            {
+                if (dict2.ContainsKey(character))
+                {
+                    int diff = dict1[character] - dict2[character];
+                    if (diff != 0)
+                    {
+                        if (diff < 0)
+                            diff = -diff;
+                        difference.Add(character, diff);
+                    }
+                }
+                else
+                    difference.Add(character, dict1[character]);
+            }
+            foreach (var character in dict2.Keys)
+                if (!dict1.ContainsKey(character))
+                    difference.Add(character, dict2[character]);
+            return difference;
+        }
 
     }
 }
