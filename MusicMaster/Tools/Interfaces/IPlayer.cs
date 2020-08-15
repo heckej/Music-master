d294881bc549535e5d85a910b20478cbeb92459d
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-15 09:09:21 +0200

    Music Player skeleton

---
+++ MusicMaster/Tools/Interfaces/IPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    interface IPlayer
+    {
+        void Play(string fileName);
+        void Pause();
+        void Resume();
+        void Stop();
+        void VolumeUp(double percentage = 10);
+        void VolumeDown(double percentage = 10);
+        void PlayNext();
+        void PlayPrevious();
+    }
+}
--- MusicMaster/Tools/MusicPlayer.cs
+++ MusicMaster/Tools/MusicPlayer.cs
@@ -1,32 +1,76 @@
 ﻿using MusicMasterBot;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
+using Tools.Players;
 
 namespace Tools
 {
-    public class MusicPlayer
+    public class MusicPlayer : IPlayer
     {
-        Song _currentSong;
-        bool _playing;
+        private Song _currentSong;
+        private IPlayer _player;
+
+        public MusicPlayer()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                _player = new LinuxPlayer();
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                _player = new WindowsPlayer();
+            else
+                throw new Exception("No implementation exists for the current OS");
+        }
 
         public void Play(Song song)
         {
             // play the song song.Title by song.Artist
             _currentSong = song;
-            _playing = true;
+            Play(song.FilePath);
         }
 
         public void Pause()
         {
             // pause current song
-            _playing = false;
+            _player.Pause();
         }
 
         public void Resume()
         {
             // resume playing
-            _playing = true;
+            _player.Resume();
         }
+
+        public void Play(string fileName)
+        {
+            _player.Play(fileName);
+        }
+
+        public void Stop()
+        {
+            _player.Stop();
+        }
+
+        public void VolumeUp(double percentage = 10)
+        {
+            _player.VolumeUp(percentage);
+        }
+
+        public void VolumeDown(double percentage = 10)
+        {
+            _player.VolumeDown(percentage);
+        }
+
+        public void PlayNext()
+        {
+            _player.PlayNext();
+        }
+
+        public void PlayPrevious()
+        {
+            _player.PlayPrevious();
+        }
+
+        public Song CurrentSong => _currentSong;
     }
 }
