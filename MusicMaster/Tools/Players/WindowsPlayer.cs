b2bf1ca95b4aab6b4ca711d054a40937a8e2ae0d
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-15 09:09:43 +0200

    Windows Player skeleton

---
+++ MusicMaster/Tools/Players/WindowsPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools.Players
+{
+    class WindowsPlayer : IPlayer
+    {
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayPrevious()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Resume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VolumeDown(double percentage = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VolumeUp(double percentage = 10)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
