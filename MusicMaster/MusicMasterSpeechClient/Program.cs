38d887c9a2145c5c76897925c8a2972609a92736
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-15 23:10:13 +0200

    Speech client basic models

---
+++ MusicMaster/MusicMasterSpeechClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MusicMasterSpeechClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Crunch.NET.Response>>(await streamTask);
+
+            var msg = await streamTask;
+            Console.Write(msg);
+        }
+    }
+}
