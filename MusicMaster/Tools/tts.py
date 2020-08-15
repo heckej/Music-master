684dbef76b81ff27f8a66b8ce301ac23bb7cc121
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-15 16:40:01 +0200

    TTS: consider all input arguments 1-last as text

--- MusicMaster/Tools/tts.py
+++ MusicMaster/Tools/tts.py
@@ -2,12 +2,10 @@ from gtts import gTTS
 import os
 import sys
 
-with file = open("tts.log", "a"):
-    file.write("test")
-
-print("hello world")
 if __name__ == "__main__":
     print(str(sys.argv))
-    tts = gTTS(text=sys.argv[1], lang='en')
-    tts.save("speak.mp3")
-    os.system("mpg321 speak.mp3")
+    text = ' '.join(sys.argv[1:])
+    if text != '':
+        tts = gTTS(text=text, lang='en')
+        tts.save("speak.mp3")
+        os.system("mpg321 speak.mp3")
