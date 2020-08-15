3110556b72490ab9395d29bc533516e92fa63c10
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-15 09:09:32 +0200

    Linux player: based on cmus-remote

---
+++ MusicMaster/Tools/Players/LinuxPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Players
+{
+    class LinuxPlayer : IPlayer
+    {
+        string _cmus = "cmus-remote";
+
+        public void Pause()
+        {
+            ExecuteCmus("-u");
+        }
+
+        public void Play(string fileName)
+        {
+            ExecuteCmus("-q " + fileName);
+            ExecuteCmus("-n");
+        }
+
+        public void PlayNext()
+        {
+            ExecuteCmus("-n");
+        }
+
+        public void PlayPrevious()
+        {
+            ExecuteCmus("-r");
+        }
+
+        public void Resume()
+        {
+            ExecuteCmus("-p");
+        }
+
+        public void Stop()
+        {
+            ExecuteCmus("-s");
+        }
+
+        public void VolumeDown(double percentage = 10)
+        {
+            ExecuteCmus("-v -" + percentage + "%");
+        }
+
+        public void VolumeUp(double percentage = 10)
+        {
+            ExecuteCmus("-v +" + percentage + "%");
+        }
+
+        private void ExecuteCmus(string arg)
+        {
+            var res = new RunCmd().Run(_cmus, arg);
+            if (res.Contains("cmus is not running"))
+            {
+                res = new RunCmd().Run("cmus & \n", "");
+                res = new RunCmd().Run(_cmus, arg);
+            }
+        }
+    }
+}
