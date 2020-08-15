01c05977e458c8dec4e35a5af104af64df2dc1fd
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-15 09:08:10 +0200

    Edit typo in welcome message

--- MusicMaster/MusicMasterBot/MusicMasterBot/Dialogs/MainDialog.cs
+++ MusicMaster/MusicMasterBot/MusicMasterBot/Dialogs/MainDialog.cs
@@ -55,7 +55,7 @@ namespace MusicMasterBot.Dialogs
             }
 
             // Use the text provided in FinalStepAsync or the default if it is the first time.
-            var messageText = stepContext.Options?.ToString() ?? "What can I help you with today?\nSay something like \"Play Someone Like Me by Adele\"";
+            var messageText = stepContext.Options?.ToString() ?? "What can I help you with today?\nSay something like \"Play Someone Like You by Adele\"";
             var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
             return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
         }
