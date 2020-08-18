69d1fd690c62965b78401cdaae4b88e63bd762ba
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-18 14:56:25 +0200

    SongRequest: ...InDatabase fields

--- MusicMaster/MusicMasterBot/MusicMasterBot/SongRequest.cs
+++ MusicMaster/MusicMasterBot/MusicMasterBot/SongRequest.cs
@@ -16,5 +16,18 @@ namespace MusicMasterBot
         public string Genre { get; set; }
 
         public  Intent Intent { get; set; }
+
+        private bool _titleInDatabase = false;
+        private bool _artistInDatabase = false;
+        private bool _genreInDatabase = false;
+        private bool _albumInDatabase = false;
+
+        public bool TitleInDatabase { get => _titleInDatabase; set => _titleInDatabase = value; }
+
+        public bool ArtistInDatabase { get => _artistInDatabase; set => _artistInDatabase = value; }
+
+        public bool GenreInDatabase { get => _genreInDatabase; set => _genreInDatabase = value; }
+
+        public bool AlbumInDatabase { get => _albumInDatabase; set => _albumInDatabase = value; }
     }
 }
