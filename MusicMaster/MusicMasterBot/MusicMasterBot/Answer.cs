d04e295942ca6699f76136a77374227d4fbde497
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-25 22:38:22 +0200

    Question/Answer objects

---
+++ MusicMaster/MusicMasterBot/MusicMasterBot/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicMasterBot
+{
+    public class Answer
+    {
+        public Answer(Question question)
+        {
+            Question = question;
+        }
+
+        public Answer() { }
+
+        public Question Question { get; private set; }
+        public string Text { get; set; }
+        public string Speak { get; set; }
+    }
+}
