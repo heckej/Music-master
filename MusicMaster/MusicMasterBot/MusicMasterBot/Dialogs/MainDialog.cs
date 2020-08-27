387562a54a09943660bcad7e9091d3c465220c37
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-08-27 17:21:45 +0200

    MainDialog: switch between song, answer results

--- MusicMaster/MusicMasterBot/MusicMasterBot/Dialogs/MainDialog.cs
+++ MusicMaster/MusicMasterBot/MusicMasterBot/Dialogs/MainDialog.cs
@@ -297,37 +297,41 @@ namespace MusicMasterBot.Dialogs
         {
             // If the child dialog ("RequestSongDialog") was cancelled, the user failed to confirm or if the intent wasn't PlayByTitleArtist
             // the Result here will be null.
-            string messageText;
-            string spokenMessageText;
-            Activity message;
-            switch (stepContext.Result)
+            if (!(stepContext.Result is null))
             {
-                case Song songToBePlayed:
-                    // Now we have all the song details call the music player.
-                    try
-                    {
-                        _musicPlayer.Play(songToBePlayed);
+                string messageText;
+                string spokenMessageText;
+                Activity message;
+                switch (stepContext.Result)
+                {
+                    case Song songToBePlayed:
+                        // Now we have all the song details call the music player.
+                        try
+                        {
+                            _musicPlayer.Play(songToBePlayed);
 
-                        // If the call to the music player service was successful tell the user.
-                        (messageText, spokenMessageText) = SentenceGenerator.PresentSongToBePlayed(songToBePlayed);
+                            // If the call to the music player service was successful tell the user.
+                            (messageText, spokenMessageText) = SentenceGenerator.PresentSongToBePlayed(songToBePlayed);
+                            message = MessageFactory.Text(messageText, spokenMessageText, InputHints.IgnoringInput);
+                        }
+                        catch (Exception e)
+                        {
+                            messageText = e.ToString();
+                            spokenMessageText = "Sorry, something went wrong, so I cannot play the song.";
+                            message = MessageFactory.Text(messageText, spokenMessageText, InputHints.IgnoringInput);
+                        }
+                        break;
+                    case Answer answer:
+                        (messageText, spokenMessageText) = (answer.Text, answer.Speak);
                         message = MessageFactory.Text(messageText, spokenMessageText, InputHints.IgnoringInput);
-                    } catch(Exception e)
-                    {
-                        messageText = e.ToString();
-                        spokenMessageText = "Sorry, something went wrong, so I cannot play the song.";
+                        break;
+                    default:
+                        (messageText, spokenMessageText) = SentenceGenerator.CommandNotUnderstood();
                         message = MessageFactory.Text(messageText, spokenMessageText, InputHints.IgnoringInput);
-                    }
-                    break;
-                case Answer answer:
-                    (messageText, spokenMessageText) = (answer.Text, answer.Speak);
-                    message = MessageFactory.Text(messageText, spokenMessageText, InputHints.IgnoringInput);
-                     break;
-                default:
-                    (messageText, spokenMessageText) = SentenceGenerator.CommandNotUnderstood();
-                    message = MessageFactory.Text(messageText, spokenMessageText, InputHints.IgnoringInput);
-                    break;
+                        break;
+                }
+                await stepContext.Context.SendActivityAsync(message, cancellationToken);
             }
-            await stepContext.Context.SendActivityAsync(message, cancellationToken);
 
             // Restart the main dialog with a different message the second time around
             var (promptMessage, _) = SentenceGenerator.ProposeFurtherHelp();
